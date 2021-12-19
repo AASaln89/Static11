c7c0ad2f216cb0c8e180ffe96f869c20e8922ea7
Andrey <Andrey@DESKTOP-TSIGHH4>
2021-12-19 22:51:43 +0300

    Добавьте файлы проекта.

---
+++ Static11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER RADIUS");
+            Console.WriteLine(Round.Okr(out double L));
+            Console.WriteLine();
+            Console.WriteLine("ENTER RADIUS");
+            Console.WriteLine(Round.S(out double S));
+            Console.ReadKey();
+        }
+    }
+    public static class Round
+    {
+        public static double Okr(out double L)
+        {
+            double r;
+            r = Convert.ToInt32(Console.ReadLine());
+            L = 2 * r * Math.PI;
+            return L;
+        }
+
+        public static double S(out double S)
+        {
+            double r;
+            r = Convert.ToInt32(Console.ReadLine());
+            S = Math.PI * r * r;
+            return S;
+        }
+    }
+}
